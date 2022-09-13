c6eb44c15bc000b7d35e2dfe83582de505f403f3
Divyesh <divyesh.bhatt23@gmail.com>
2022-09-13 23:26:28 +0530

    Initial commit

---
+++ Switch_Bug/Switch_Bug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Switch_Bug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Switch_Bug/Switch_Bug/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Switch_Bug;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	// This not works in iOS, works fine in Android
+    void toggle2_Toggled(System.Object sender, Microsoft.Maui.Controls.ToggledEventArgs e)
+    {
+		if (e.Value)
+		{
+			toggle2.OnColor = Color.FromArgb("#ffa500");
+            toggle2.ThumbColor = Color.FromArgb("#512BD4");
+        }
+		else
+		{
+            toggle2.OnColor = Color.FromArgb("#969696");
+            toggle2.ThumbColor = Color.FromArgb("#DFD8F7");
+        }
+    }
+}
+
+
